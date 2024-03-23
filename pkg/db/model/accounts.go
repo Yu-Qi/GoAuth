8d4bbdd46035a5387e4ad13b3951268cbbbd302f
Yu-Qi <alanchen@kryptogo.com>
2024-03-23 11:22:13 +0800

    feat: soft delete account

--- pkg/db/model/accounts.go
+++ pkg/db/model/accounts.go
@@ -7,12 +7,13 @@ const TableNameAccount = "accounts"
 
 // Account mapped from table <accounts>
 type Account struct {
-	UID            string    `gorm:"column:uid;type:varchar(36);not null;primaryKey"`
-	Email          string    `gorm:"column:email;type:varchar(256);not null;uniqueIndex:idx_accounts"`
-	HashedPassword string    `gorm:"column:hashed_password;type:varchar(72);not null"`
-	IsActive       bool      `gorm:"column:is_active;type:tinyint(1);not null;default:0"`
-	CreatedAt      time.Time `gorm:"column:created_at;type:timestamp;not null;default:CURRENT_TIMESTAMP"`
-	UpdatedAt      time.Time `gorm:"column:updated_at;type:timestamp;not null;default:CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"`
+	UID            string     `gorm:"column:uid;type:varchar(36);not null;primaryKey"`
+	Email          string     `gorm:"column:email;type:varchar(256);not null;uniqueIndex:idx_accounts"`
+	HashedPassword string     `gorm:"column:hashed_password;type:varchar(72);not null"`
+	IsActive       bool       `gorm:"column:is_active;type:tinyint(1);not null;default:0"`
+	CreatedAt      time.Time  `gorm:"column:created_at;type:timestamp;not null;default:CURRENT_TIMESTAMP"`
+	UpdatedAt      time.Time  `gorm:"column:updated_at;type:timestamp;not null;default:CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"`
+	DeleteAt       *time.Time `gorm:"column:delete_at;type:timestamp"`
 }
 
 // TableName Account's table name
